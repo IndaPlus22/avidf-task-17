b3b2c9b183873e51dbcd72fddcd6c4ebdad79995
HotFazz <basalts.darter-0a@icloud.com>
2023-02-16 19:32:58 +0100

    sorry again

--- Labb_2/src/Main.java
+++ Labb_2/src/Main.java
@@ -2,31 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    // Minimum of three values
+    // Fin the minimum of the three values
     static int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
     }
 
-    // Calculates the Levenshtein distance between two strings dynamically bottom-up instead of recursively top-down,
-    // resulting in linear time complexity instead of exponential
+
     static int calc_edit_distance(String str1, String str2) {
         // str1 spans the left side while str2 spans the top
         int m = str1.length() + 1;
         int n = str2.length() + 1;
         int[][] matrix = new int[m][n];
 
-        // Prefixes of str1 can be transformed into the empty string (0th prefix of str2)
-        // By removing all characters
+
         for (int i = 1; i < m; i++) {
             matrix[i][0] = i;
         }
 
-        // Empty str1 can be transformed into the prefixes of str2 by adding every character
+
         for (int j = 1; j < n; j++) {
             matrix[0][j] = j;
         }
 
-        // Fill remaining matrix with edit distances between the substrings in matrix[i][j]
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 int replace_cost = 0;
@@ -34,7 +32,7 @@ public class Main {
                     replace_cost = 1;
                 }
 
-                // This cell's edit distance is the minimum out of the three possible edits
+
                 matrix[i][j] = min(
                         matrix[i - 1][j] + 1,                 // Deletion
                         matrix[i][j - 1] + 1,                // Insertion
@@ -52,7 +50,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String word = scanner.nextLine().trim();
-            if (word.equals("#")) { // End of word list
+            if (word.equals("#")) {
                 break;
             }
             wordList.add(word);
@@ -62,10 +60,9 @@ public class Main {
         while (scanner.hasNextLine()) {
             String misspelledWord = scanner.nextLine().trim();
             int shortestDistance = Integer.MAX_VALUE;
-            ArrayList<String> matchingWords = new ArrayList<>(); // Words with lowest edit distance
+            ArrayList<String> matchingWords = new ArrayList<>();
 
-            // Calculate edit distance between misspelled word and every word in the word list
-            // Save the ones with the lowest edit distance
+
             for (String word : wordList) {
                 int distance = calc_edit_distance(misspelledWord, word);
 
